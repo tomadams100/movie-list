64cb406692b509c5aee843513bf106cbf1886fcc
Tom Adams <thomasjeadams@btinternet.com>
2022-01-09 19:53:27 +0100

    added require path

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path")
 const User = require("./models/Users.model");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
