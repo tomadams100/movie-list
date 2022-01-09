1a73126d2b7753be807e11db85e206035b774607
Tom Adams <thomasjeadams@btinternet.com>
2022-01-09 19:25:53 +0100

    modified package lock for heroku deployment

--- client/src/App.js
+++ client/src/App.js
@@ -7,18 +7,15 @@ import Layout from './components/Layout';
 import About from './components/About';
 import All_Movies from './components/All_Movies';
 import My_Movies from './components/My_Movies';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { Routes, Route, Outlet } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import Movie_Details from './components/Movie_Details';
 import Signup from './components/Signup';
 import Login from './components/Login';
-import axios from 'axios';
 import PrivateRoute from './components/PrivateRoute';
 import AnonRoute from './components/AnonRoute';
 
 function App(props) {
-const [data, setData] = useState(null)
   return (
     <div className="App">
     <Layout />
